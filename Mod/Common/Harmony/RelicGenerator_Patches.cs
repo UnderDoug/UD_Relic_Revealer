ac69e7749e147b4a3614fc1077125e44d126ddf4
UnderDoug <douglas.marken@gmail.com>
2026-08-25 19:15:10 +1000

    Added patch to ensure reliquary relics get the name generated for them during worldgen (may swap to transpiler down the track)

---
+++ Mod/Common/Harmony/RelicGenerator_Patches.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+
+using System;
+
+using HistoryKit;
+using XRL.World;
+
+namespace UD_Relic_Revealer.Mod.Harmony
+{
+    [HarmonyPatch(typeof(RelicGenerator))]
+    public static class RelicGenerator_Patches
+    {
+        [HarmonyPatch(
+            declaringType: typeof(RelicGenerator),
+            methodName: nameof(RelicGenerator.GenerateRelic),
+            argumentTypes: new Type[]
+            {
+                typeof(HistoricEntitySnapshot),
+                typeof(string),
+                typeof(bool),
+            },
+            argumentVariations: new ArgumentType[]
+            {
+                ArgumentType.Normal,
+                ArgumentType.Normal,
+                ArgumentType.Normal,
+            })]
+        [HarmonyPrefix]
+        public static bool GenerateRelic_PassName_Prefix(
+            ref GameObject __result,
+            HistoricEntitySnapshot Snapshot,
+            string Type = null,
+            bool RandomName = false
+            )
+        {
+            int tier = RelicGenerator.GetRelicTierFromPeriod(int.Parse(Snapshot.GetProperty("period")));
+            __result = RelicGenerator.GenerateRelic(Snapshot, tier, Type, RandomName);
+            return __result == null; // fall back to the un-patched method if the relic fails to generate; otherwise, skip it
+        }
+    }
+}

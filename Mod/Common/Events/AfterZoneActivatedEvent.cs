45a00c76a7210eb7f2403b635a9aa34ad84dddbb
UnderDoug <douglas.marken@gmail.com>
2026-08-25 19:11:50 +1000

    Added event for after a zone is activated.

---
+++ Mod/Common/Events/AfterZoneActivatedEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using XRL;
+using XRL.World;
+
+namespace UD_Relic_Revealer.Mod.Events
+{
+    public class AfterZoneActivatedEvent : ModSingletonEvent<AfterZoneActivatedEvent>
+    {
+        public static readonly string RegisteredEventID = nameof(AfterZoneActivatedEvent);
+
+        public new static readonly int CascadeLevel = CASCADE_ALL;
+
+        public Zone Zone;
+
+        public AfterZoneActivatedEvent()
+            : base()
+        { }
+
+        public override int GetCascadeLevel()
+            => CascadeLevel
+            ;
+
+        public virtual string GetRegisteredEventID()
+            => RegisteredEventID
+            ;
+
+        public override void Reset()
+        {
+            base.Reset();
+            Zone = null;
+        }
+
+        public static void Send(Zone Zone)
+        {
+            Instance.Zone = Zone;
+            The.Game.HandleEvent(Instance);
+            Zone.HandleEvent(Instance);
+            Instance.Reset();
+        }
+    }
+}
---
+++ Mod/Common/Harmony/Zone_Patches.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+
+using System;
+
+using XRL.World;
+
+using UD_Relic_Revealer.Mod.Events;
+
+namespace UD_Relic_Revealer.Mod.Harmony
+{
+    [HarmonyPatch(typeof(Zone))]
+    public static class Zone_Patches
+    {
+        [HarmonyPatch(
+            declaringType: typeof(Zone),
+            methodName: nameof(Zone.Activated))]
+        [HarmonyPostfix]
+        public static void ZoneSend_IncludeAfter_Postfix(ref Zone __instance)
+        {
+            AfterZoneActivatedEvent.Send(__instance);
+        }
+
+        [HarmonyPatch(
+            declaringType: typeof(InteriorZone),
+            methodName: nameof(InteriorZone.Activated))]
+        [HarmonyPostfix]
+        public static void InteriorZoneSend_IncludeAfter_Postfix(ref Zone __instance)
+        {
+            AfterZoneActivatedEvent.Send(__instance);
+        }
+    }
+}
